eb02f3bbf11e98b1a2230d6d1dee8597fe5a8556
Dimas Begunoff <dbegunoff@gmail.com>
2014-11-06 16:14:31 -0800

    each callback returns array of loaded images

--- jquery.imgpreload.js
+++ jquery.imgpreload.js
@@ -8,33 +8,13 @@ https://github.com/farinspace/jquery.imgpreload
 Licensed under the MIT license
 http://en.wikipedia.org/wiki/MIT_License
 
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
 */
 
 if ('undefined' != typeof jQuery)
 {
 	(function($){
-
+		'use strict';
+		
 		// extend jquery (because i love jQuery)
 		$.imgpreload = function (imgs,settings)
 		{
@@ -66,8 +46,8 @@ if ('undefined' != typeof jQuery)
 					loaded.push(img_obj);
 
 					$.data(img_obj, 'loaded', ('error'==e.type)?false:true);
-					
-					if (settings.each instanceof Function) { settings.each.call(img_obj); }
+
+					if (settings.each instanceof Function) { settings.each.call(img_obj, loaded.slice()); }
 
 					// http://jsperf.com/length-in-a-variable
 					if (loaded.length>=imgs.length && settings.all instanceof Function) { settings.all.call(loaded); }
@@ -88,67 +68,9 @@ if ('undefined' != typeof jQuery)
 
 		$.fn.imgpreload.defaults =
 		{
-			each: null // callback invoked when each image in a group loads
-			, all: null // callback invoked when when the entire group of images has loaded
+			each: null // callback invoked when each image is loaded
+			, all: null // callback invoked when all images have loaded
 		};
 
 	})(jQuery);
 }
-
-/*
-
-	Usage:
-
-	$('#content img').imgpreload(function()
-	{
-		// this = array of dom image objects
-		// callback executes when all images are loaded
-	});
-
-	$('#content img').imgpreload
-	({
-		each: function()
-		{
-			// this = dom image object
-			// check for success with: $(this).data('loaded')
-			// callback executes when each image loads
-		},
-		all: function()
-		{
-			// this = array of dom image objects
-			// check for success with: $(this[i]).data('loaded')
-			// callback executes when all images are loaded
-		}
-	});
-
-	$.imgpreload('/images/a.gif',function()
-	{
-		// this = array of dom image objects
-		// check for success with: $(this[i]).data('loaded')
-		// callback
-	});
-
-	$.imgpreload(['/images/a.gif','/images/b.gif'],function()
-	{
-		// this = array of dom image objects
-		// check for success with: $(this[i]).data('loaded')
-		// callback executes when all images are loaded
-	});
-
-	$.imgpreload(['/images/a.gif','/images/b.gif'],
-	{
-		each: function()
-		{
-			// this = dom image object
-			// check for success with: $(this).data('loaded')
-			// callback executes on every image load
-		},
-		all: function()
-		{
-			// this = array of dom image objects
-			// check for success with: $(this[i]).data('loaded')
-			// callback executes when all images are loaded
-		}
-	});
-
-*/
